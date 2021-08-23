0aacdfe69460184527e3080772ba430be7cc5682
Kanemura775 <milkchocolate0141775@gmail.com>
2021-08-23 17:44:36 +0900

    投稿機能/枠

---
+++ src/components/SubmitForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form__wrapper">
+    <textarea
+      class="form__textarea"
+      v-model="text"
+      placeholder="冷蔵庫にある食材"
+    />
+    <div class="form__buttons">
+      <button v-on:click="post" class="form__submit-button">記録</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    }
+  },
+  methods: {
+    post() {
+      alert("なにたべる？")
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form__wrapper {
+  padding: 1rem;
+}
+.form__textarea {
+  width: 100%;
+  height: 50px;
+  padding: 0.3rem;
+  line-height: 1.3rem;
+  border-radius: 5px;
+  border: none;
+  resize: none;
+}
+.form__textarea:focus {
+  outline: none;
+}
+.form__buttons {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
